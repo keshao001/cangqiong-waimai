d091a52d2f4abff9276316aac471c423645ff6a0
zy <zy2000@stu.pku.edu.cn>
2024-03-16 20:07:24 +0800

    苍穹外卖（新增员工+分页查询+启用禁用员工账号）

---
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -0,0 +1,44 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface EmployeeMapper {
+
+    /**
+     * 根据用户名查询员工
+     * @param username
+     * @return
+     */
+    @Select("select * from employee where username = #{username}")
+    Employee getByUsername(String username);
+
+    /**
+     * 插入员工数据
+     * @param employee
+     */
+    @Insert("insert into employee" +
+            "(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user)" +
+            "VALUES " +
+            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime},#{updateTime},#{createUser}, #{updateUser})")
+   void insert(Employee employee);
+
+    /**
+     * 分页查询方法
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+
+    /**
+     * 根据主键动态修改属性
+     * @param employee
+     */
+    void update(Employee employee);
+}
